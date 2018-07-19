dc9b7eeef133829a1a54ea74c1bf1f6da3854f14
Guillaum-C <guillaum@jlab.org>
2018-07-19 16:10:14 +0200

    Remove cuts and detector plots from this project

--- src/org/clas12/analysisTools/cuts/particles/PhotonCut.java
+++
@@ -1,7 +0,0 @@
-package org.clas12.analysisTools.cuts.particles;
-
-public class PhotonCut extends ParticleCut{
-	
-	
-	
-}
--- src/org/clas12/analysisTools/cuts/particles/ProtonCut.java
+++
@@ -1,5 +0,0 @@
-package org.clas12.analysisTools.cuts.particles;
-
-public class ProtonCut extends ParticleCut {
-	
-}
